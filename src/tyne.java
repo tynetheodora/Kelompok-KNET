97c0780dd7573a31378fe978d231f633619a87fa
tynetheodora <tynetheodora1111@gmail.com>
2023-05-24 11:47:56 +0700

    Create tyne.java

---
+++ src/tyne.java
@@ -0,0 +1,10 @@
+public class tyne {
+    public static void main(String[] args) {
+        int count = 1;
+
+        while (count <= 5) {
+            System.out.println("Perulangan ke-" + count);
+            count++;
+        }
+    }
+}
