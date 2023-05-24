8e0ec1e0e0de9db73c1ff2765d166f9f0cecb5e9
evelline20 <134467045+evelline20@users.noreply.github.com>
2023-05-24 14:33:29 +0700

    Update Evelline.java

---
+++ src/Evelline.java
@@ -0,0 +1,13 @@
+// Evelline CHristine
+// 03081220027
+
+public class Evelline {
+    public static void main(String[] args) {
+        System.out.println("Perulangan 1 sampai 10");
+        int count = 1;
+        while (count <= 10) {
+            System.out.println("Perulangan ke-" + count);
+            count++;
+        }
+    }
+}
