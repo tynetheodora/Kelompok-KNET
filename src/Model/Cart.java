f8e89149f798775775c101983e41e67c6fcc4464
tynetheodora <tynetheodora1111@gmail.com>
2023-05-29 11:35:24 +0700

    dashboard
    tampilan dashboard

--- src/Model/Cart.java
+++ src/Model/Cart.java
@@ -1,40 +1,12 @@
 package model;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
-class Cart {
-    private String name;
-    private double price;
-
-    public Cart(String name, double price) {
-        this.name = name;
-        this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-}
-
-class CartItem {
-    private Cart product;
-    private int quantity;
-
-    public CartItem(Cart product, int quantity) {
-        this.product = product;
-        this.quantity = quantity;
-    }
-
-    public Cart getProduct() {
-        return product;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-}
+/* dalam CART memiliki atribut berikut :
+ * product/product name
+ * kuantitas/jumlah
+ * harga
+ * sub-total
+ * diskon
+ * total harga
+ * checkout
+ * notes
+ */
