ef3c2d973488c724a4b449f05302e42c2547dba9
evelline20 <134467045+evelline20@users.noreply.github.com>
2023-05-29 11:08:10 +0700

    dekomposisi delivery dan user

--- src/Model/User.java
+++ src/Model/User.java
@@ -4,6 +4,16 @@ package model;
  */
 //evelline 03081220027
  public class User {
-    
+    /*
+     * Deskripsi:
+     * Atribut
+     * id : ID pengguna
+     * username : untuk menyimpan nama pengguna
+     * email : untuk menyimpan alamat email pengguna
+     * password :untuk menyimpan kata sandi pengguna
+     * phoneNumber : untuk menyimpan nama pelanggan
+     * Task
+     * User dapat mengedit informasi pribadi
+     */
  }
  
--- src/model/DeliveryTracking.java
+++ src/model/DeliveryTracking.java
@@ -1,5 +1,13 @@
 package model;
 //evelline 03081220027
 public class DeliveryTracking {
-    
+    /*
+     * Deskripsi:
+     * Atribut
+     * trackingNumber : untuk nomor pelacakan pengiriman
+     * status : untuk status pengiriman
+     * 
+     * Task
+     * User bisa memantau orderan yang dipesan sudah sampai mana
+     */
 }
