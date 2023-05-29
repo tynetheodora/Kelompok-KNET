7255a06a605294e4af1920ebdad1ecb8551ac4e9
evelline20 <134467045+evelline20@users.noreply.github.com>
2023-05-29 11:46:21 +0700

    Update User.java

--- src/Model/User.java
+++ src/Model/User.java
@@ -10,7 +10,7 @@ package model;
      * id : ID pengguna
      * username : untuk menyimpan nama pengguna
      * email : untuk menyimpan alamat email pengguna
-     * password :untuk menyimpan kata sandi pengguna
+     * password : untuk menyimpan kata sandi pengguna
      * phoneNumber : untuk menyimpan nama pelanggan
      * Task
      * User dapat mengedit informasi pribadi
