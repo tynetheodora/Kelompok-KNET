10cb7d845cd60ad7d5a93fc71739dc118f764f9e
kylnmr <kaylanamra@gmail.com>
2023-05-29 11:38:07 +0700

    Update Payment.java

--- src/model/Payment.java
+++ src/model/Payment.java
@@ -1,8 +1,23 @@
 package model;
 
+/* Nama : Kayla Namira
+ * NIM : 03081220026
+ */
+
 public class Payment {
-    public static void main(String[] args) throws Exception {
-        
-    }
-    
+    /*
+     * Deskripsi
+     * Atribut
+     * amount: jumlah orderan
+     * currency: IDR
+     * paymentMethod : transfer bank, cash
+     * transactionDate : waktu transaksi pembayaran
+     * transactionId : ID transaksi pembayaran
+     * status: status payment berhasil, proses, gagal
+     * additionalDetails : informasi tambahan(detail alamat, dll)
+     * 
+     * task
+     * memproses transaksi pembayaran
+     * 
+     */
 }
