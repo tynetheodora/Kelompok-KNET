a98fc592043a9ccebabaa9b14e7784bede2e4a20
tynetheodora <tynetheodora1111@gmail.com>
2023-05-29 11:33:36 +0700

    dashboard
    tampilan dashboard

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
+<<<<<<< Updated upstream
     public static void main(String[] args) throws Exception {
         
     }
+=======
+/* dalam tampilan Menu-Dashboard memiliki atribut berikut :
+ * items
+ * penawaran
+ * delivery
+ * akun
+ * member
+ * order
+ * alamat pengiriman
+ * history
+ * favourites
+ */
+>>>>>>> Stashed changes
 }
--- src/Model/Cart.java
+++ src/Model/Cart.java
@@ -1,5 +1,40 @@
 package model;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class Cart {
-    
+class Cart {
+    private String name;
+    private double price;
+
+    public Cart(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
+class CartItem {
+    private Cart product;
+    private int quantity;
+
+    public CartItem(Cart product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Cart getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
 }
