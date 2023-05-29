4e71d833b27c2733925e5a9968b2ba78415cf7e7
kylnmr <kaylanamra@gmail.com>
2023-05-29 10:54:02 +0700

    Update payment.java

--- src/Model/payment.java
+++ src/Model/payment.java
@@ -1,5 +1,6 @@
 package model;
 
+<<<<<<< HEAD
 /* Nama : Kayla Namira
  * NIM : 03081220026
  */
@@ -8,4 +9,8 @@ public class Payment {
     public static void main(String[] args) throws Exception {
         
     }
+=======
+public class payment {
+    
+>>>>>>> 155d1af5e90e0df0cc59ddb43a1e84aa7a793523
 }
