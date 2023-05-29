289de7f011064b38903f51c8ffc7cf596e6d0cf1
tynetheodora <134466861+tynetheodora@users.noreply.github.com>
2023-05-29 11:37:02 +0700

    Add files via upload

---
+++ src/model/Cart.java
@@ -0,0 +1,12 @@
+package model;
+
+/* dalam CART memiliki atribut berikut :
+ * product/product name
+ * kuantitas/jumlah
+ * harga
+ * sub-total
+ * diskon
+ * total harga
+ * checkout
+ * notes
+ */
