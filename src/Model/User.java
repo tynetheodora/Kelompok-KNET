58ae76891151910651ac54e90a67735c000c96ad
evelline20 <134467045+evelline20@users.noreply.github.com>
2023-05-31 11:16:40 +0700

    Update User.java

--- src/Model/User.java
+++ src/Model/User.java
@@ -3,7 +3,7 @@ package model;
  * 
  */
 //evelline 03081220027
- public class User {
+ 
     /*
      * Deskripsi:
      * Atribut
@@ -14,6 +14,45 @@ package model;
      * phoneNumber : untuk menyimpan nama pelanggan
      * Task
      * User dapat mengedit informasi pribadi
-     */
- }
+     */   
+    
+     public class User {
+        private String id;
+        private String username;
+        private String email;
+        private String password;
+        
+        public String getId() {
+            return id;
+        }
+        
+        public void setId(String id) {
+            this.id = id;
+        }
+        
+        public String getUsername() {
+            return username;
+        }
+        
+        public void setUsername(String username) {
+            this.username = username;
+        }
+        
+        public String getEmail() {
+            return email;
+        }
+        
+        public void setEmail(String email) {
+            this.email = email;
+        }
+        
+        public String getPassword() {
+            return password;
+        }
+        
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+ 
  
