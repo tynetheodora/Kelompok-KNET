2ac61cbb8380d3365a4532b65d5723c0c21c1425
Novitalim <novitalim04@gmail.com>
2023-05-31 11:11:42 +0700

    penambahan data

--- src/model/Order.java
+++ src/model/Order.java
@@ -2,9 +2,25 @@ package model;
 
 /*
  * didalam icon order , memiliki atribut:
+ * common attribut
  * - edit pesanan
  * - lihat rating pesanan
  * - search bar
  * 
  * 
+ * 
+ * detail attribut
+ * - order detail
+ * - order number
+ * - order Time
+ * - order queue
+ * 
+ * 
+ * 
+ * 
  */
+
+ String nama;
+ String studentId;
+ String alamat;
+ String programStudi;
