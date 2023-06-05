2af249d21418ed8f7c25822016b264c11f11342b
kylnmr <kaylanamra@gmail.com>
2023-06-05 11:21:23 +0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,19 +1,28 @@
+import model.User;
+
 public class App {
-<<<<<<< Updated upstream
     public static void main(String[] args) throws Exception {
-        
+       init();
+        String namaUser = "Farrel";
+        String namaKuliah ="Matematika Diskrit";
+        int semester =1;
+        String nilaiMatakuliah = "D";
+
+        Mahasiswa farrel=new Mahasiswa();
+        System.out.println(farrel);
+        farrel = new Mahasiswa("Farrel", "Jl. Medan");
+        System.out.println(farrel);
+        Mahasiswa jerry = new Mahasiswa("Jerry","030081211",
+                    "Jl.jl Medan","Sistem Informasi");
+        System.out.println(jerry);
     }
-=======
-/* dalam tampilan Menu-Dashboard memiliki atribut berikut :
- * items
- * penawaran
- * delivery
- * akun
- * member
- * order
- * alamat pengiriman
- * history
- * favourites
- */
->>>>>>> Stashed changes
+    public static void init(){
+        Mahasiswa Tyne = new Mahasiswa("Tyne Theodora","030081211",
+                    "Jl.jl Medan","Sistem Informasi");
+    }
+    public static void cetakMahasiswa(Mahasiswa x){
+        System.out.println(x.getNama() + " "+ x.getAlamat());
+    }
+    public void hitungIPK(){}
+    public void retake(){}
 }
