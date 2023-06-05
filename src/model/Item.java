aaa52b924cf1927bee885ac13adb490af4661fef
tynetheodora <tynetheodora1111@gmail.com>
2023-06-05 12:34:43 +0700

    item.java
    update item

--- src/model/Item.java
+++ src/model/Item.java
@@ -1,5 +1,50 @@
 package model;
 
 public class Item {
+    String food;
+    String drink;
+    String dessert;
 
+    public Item (){
+
+    }
+
+    public Item (String food, String drink, String dessert){
+        this.food = food;
+        this.drink = drink;
+        this.dessert = dessert;
+    }
+
+    @Override
+    public String toString() {
+        return "|" +
+            " " + getFood() + "\t|" +
+            " " + getDrink() + "\t|" +
+            " " + getDessert() + "\t|" +
+            "";
+    }
+
+    public String getFood() {
+        return this.food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public String getDrink() {
+        return this.drink;
+    }
+
+    public void setDrink(String drink) {
+        this.drink = drink;
+    }
+
+    public String getDessert() {
+        return this.dessert;
+    }
+
+    public void setDessert(String dessert) {
+        this.dessert = dessert;
+    }
 }
