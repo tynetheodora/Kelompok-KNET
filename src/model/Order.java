4e0e0c907ee6f9dbf0a7ae92d8b57b930daac66e
tynetheodora <tynetheodora1111@gmail.com>
2023-06-21 09:36:25 +0700

    Update .DS_Store

---
+++ src/model/Order.java
@@ -0,0 +1,115 @@
+package model;
+
+public class Order {
+        /* Attributes :
+        * Nama Restoran
+        * Alamat Restoran
+        * Nama Pengguna
+        * ID Pemesanan
+        * Tanggal Pembelian
+        * Jam Pembelian
+        * 
+        * TASK : memberi informasi mengenai pemesanan
+        */
+
+    String namaRestoran;
+    String alamatRestoran;
+    String namaPengguna;
+    String idPemesanan;
+    String tanggalPembelian;
+    String jamPembelian;
+    
+    public Order (){
+
+    }
+        
+    public Order ( String namaRestoran, String alamatRestoran, String namaPengguna, String idPemesanan, String tanggalPembelian, String jamPembelian){
+    this.namaRestoran = namaRestoran;
+    this.alamatRestoran = alamatRestoran;
+    this.namaPengguna = namaPengguna;
+    this.idPemesanan = idPemesanan;
+    this.jamPembelian = jamPembelian;
+    
+    }
+
+
+    public Order(String namaRestoran2, String alamatRestoran2, String namaPelanggan, String idPemesanan2,
+            String tanggalPembelian2) {
+    }
+
+    public String getNamaRestoran() {
+        return this.namaRestoran;
+    }
+
+    public void setNamaRestoran(String namaRestoran) {
+        this.namaRestoran = namaRestoran;
+    }
+
+    public String getAlamatRestoran() {
+        return this.alamatRestoran;
+    }
+
+    public void setAlamatRestoran(String alamatRestoran) {
+        this.alamatRestoran = alamatRestoran;
+    }
+
+    public String getNamaPengguna() {
+        return this.namaPengguna;
+    }
+
+    public void setNamaPengguna(String namaPengguna) {
+        this.namaPengguna = namaPengguna;
+    }
+
+    public String getIdPemesanan() {
+        return this.idPemesanan;
+    }
+
+    public void setIdPemesanan(String idPemesanan) {
+        this.idPemesanan = idPemesanan;
+    }
+
+    public String getTanggalPembelian() {
+        return this.tanggalPembelian;
+    }
+
+    public void setTanggalPembelian(String tanggalPembelian) {
+        this.tanggalPembelian = tanggalPembelian;
+    }
+
+    public String getJamPembelian() {
+        return this.jamPembelian;
+    }
+
+    public void setJamPembelian(String jamPembelian) {
+        this.jamPembelian = jamPembelian;
+    }
+
+            @Override
+        public String toString() {
+
+            return "Order{" +
+                    "Nama Restoran='" + namaRestoran + '\'' +
+                    ", Alamat Restoran ='" + alamatRestoran + '\'' +
+                    ", Nama Pengguna='" + namaPengguna + '\'' +
+                    ", ID Pemesanan='" + idPemesanan + '\'' +
+                    ", Tanggal ='" + tanggalPembelian + '\'' +
+                    ", Jam ='" + jamPembelian + '\'' +
+                    '}';
+        }
+
+        public static void main(String[] args) {
+            Order Order1 = new Order();
+            Order1.setNamaRestoran("Online Order");
+            Order1.setAlamatRestoran("Polonia Medan");
+            Order1.setNamaPengguna("Tyne Theodora");
+            Order1.setIdPemesanan("1220028");
+            Order1.setTanggalPembelian("11 November 2023");
+            Order1.setJamPembelian("11:11");
+    
+            System.out.println(Order1.toString());
+
+            Order Order2 = new Order("Online Order", "Polonia Medan", "Tyne Theodora", "1220028", "11 November 2023", "11:11");
+            System.out.println(Order2.toString());
+        }
+    }
